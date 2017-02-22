c638094b94050762c31adda589424b543c819dc5
Jakub Rupinski <rupek1995@gmail.com>
2017-02-22 19:46:32 +0100

    Improve dodawanie.c:
        - Add error handling to dodawanie.c
        - Add neccessary libraries
        - Add executable file

--- lab 1/dodawanie.c
+++ lab 1/dodawanie.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
+    if (argc!=3)
+    {
+        fprintf(stderr, "usage: ./dodawanie a b\n");
+        return 1;
+    }
+
     int a = atoi(argv[1]),
         b = atoi(argv[2]);
 
     printf("suma = %d\n", a+b );
+    return 0;
 }
